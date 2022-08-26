7f3a14a7bdcc6f97eca16cf38528f05afafd2ecc
Ricardo Cassiano <rc.cassiano04@gmail.com>
2022-08-26 17:20:44 -0300

    Add files via upload

---
+++ postgresql11-instalar-codigo-fonte-alternativa.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+#
+# Instalar o PostgreSQL código fonte no Debian Gnu/Linux 11
+# ao lado de uma instalação existente.
+# 
+#
+# Autor: Ricardo Cassiano
+# E-mail: rc.cassiano04@outlook.com
+# Github: https://github.com/ricardocassiano04/
+
+echo '
+Script para compilar e instalar o PostgreSQL 11 no Debian Gnu/Linux 11.
+
+Código fonte disponível em https://www.postgresql.org/ftp/source/
+
+Esse script é adaptado da documentação oficial disponível em 
+
+https://www.postgresql.org/docs/11/installation.html
+
+Você pode modificá-lo de acordo com as duas necessidades.
+
+'
+
+
+# Instalação das dependências necessárias.
+
+sudo apt-get -y install  bison flex llvm clang zlib1g-dev \
+lib{ssl,systemd,readline,xslt1,xml2}-dev m4 make autoconf \
+pkgconf flex gcc make guile-2.2-dev patch automake
+
+# Download e extração do código fonte
+
+wget -c https://ftp.postgresql.org/pub/source/v11.16/postgresql-11.17.tar.gz
+
+tar -xf postgresql-11.17.tar.gz
+
+# Configuração das opções de compilação.
+
+cd postgresql-11.17
+
+
+CXX=/usr/bin/g++ PYTHON=python3 ./configure \
+--prefix=/usr/local/pgsql/11 \
+--with-pgport=5433 \
+--with-python \
+--with-openssl \
+--with-systemd \
+--with-libxml \
+--with-libxslt
+
+# Execução do make 
+# compilação e instalação (pode levar vários minutos, dependendo das configurações do computador).
+
+make world
+
+sudo make install-world
+
+cd src/interfaces/libpq
+
+make
+
+sudo make install
+
+
+# Altera o dono da pasta de instalação para o usuário postgres
+
+
+sudo chown -R postgres:postgres /usr/local/pgsql
+
+
+
+# Criar o serviço systemd
+
+sudo tee -a /etc/systemd/system/postgresql11.service>>/dev/null<<EOF
+
+[Unit]
+Description=PostgreSQL 11 database server
+Documentation=man:postgres(1)
+
+[Service]
+Type=notify
+User=postgres
+ExecStart=/usr/local/pgsql/11/bin/postgres -D /usr/local/pgsql/11/data
+ExecReload=/bin/kill -HUP $MAINPID
+KillMode=mixed
+KillSignal=SIGINT
+TimeoutSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Deixar o serviço desabilitado por padrão
+
+sudo systemctl disable postgresql11.service
+
+sudo systemctl daemon-reload
+
+
+echo 'Instalação completada!'
+
+
+
+
+
