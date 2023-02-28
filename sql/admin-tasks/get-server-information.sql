dca6722e841c8fa9ca4bf9c0e23b1c175a628d19
Ricardo Cassiano <rc.cassiano04@gmail.com>
2023-02-27 21:16:15 -0300

    New queries added

--- sql/admin-tasks/get-server-information.sql
+++ sql/admin-tasks/get-server-information.sql
@@ -8,15 +8,64 @@ Some basic queries to get postgresql server information.
 */
 
 
--- Get data location
+-- data location
 
 SHOW data_directory;
 
--- Get pg_hba.conf location
+-- pg_hba.conf location
 
 SHOW hba_file;
 
 
--- Get postgresql.conf location
+-- postgresql.conf location
 
 SHOW config_file;
+
+-- postgresql config
+
+SELECT *
+FROM pg_config;
+
+-- postgresql settings
+
+SELECT *
+FROM pg_settings;
+
+
+-- return server port
+
+SELECT inet_server_port();
+
+-- current database
+
+SELECT current_database();
+
+-- current user
+
+SELECT current_user;
+
+-- returns the server IP 
+-- (returns NULL if the connection is via socket)
+
+SELECT inet_server_addr(); 
+
+-- server version
+
+SELECT version();
+
+-- server uptime
+
+SELECT pg_postmaster_start_time(); -- date/time server start
+SELECT date_trunc('second', current_timestamp - pg_postmaster_start_time()) as uptime; -- time active
+
+-- query cluster databases (\l in psql)
+SELECT datname 
+FROM pg_database;
+
+
+-- count tables
+
+SELECT count(*) 
+FROM information_schema.tables 
+WHERE table_schema NOT IN ('information_schema', 'pg_catalog');
+
