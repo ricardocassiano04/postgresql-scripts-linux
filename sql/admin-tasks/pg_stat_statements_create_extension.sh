afb3232c65f4fdf6d266afbaee47db5b5d91d231
Ricardo Cassiano <rc.cassiano04@gmail.com>
2023-05-26 21:13:01 -0300

    Adjust comments

--- sql/admin-tasks/pg_stat_statements_create_extension.sh
+++ sql/admin-tasks/pg_stat_statements_create_extension.sh
@@ -2,12 +2,12 @@
 #
 # Author: Ricardo Cassiano
 #
-# Create pg_stat_statements postgresql extension in all databases at once (including template1)
+# Create pg_stat_statements postgresql extension in all databases at once (except for template0)
 # 
 # 
-# Must be executed with postgres user on linux with trust authentication method activated
-# or 
-# 
+# Must be executed with postgres user on linux with trust authentication method (https://www.postgresql.org/docs/15/auth-trust.html)
+# or authentication configured at .pgpass file (https://www.postgresql.org/docs/current/libpq-pgpass.html)
+#  
 
 databases_to_alter=$(psql -U postgres -d postgres -A -t -c " select datname from pg_database where datname <> 'template0';")
 for bases in ${databases_to_alter}
