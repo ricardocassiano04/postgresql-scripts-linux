7e8055cc5307d477571f246005ccaa46808c6586
Ricardo Cassiano <rc.cassiano04@gmail.com>
2023-08-09 21:23:19 -0300

    Ask user for instalation folder. Change build directory to instalation folder. On required packages, choose guile-3 instead of guile-2

--- install/postgresql-install-for-test.sh
+++ install/postgresql-install-for-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This bash script compiles PostgreSQL from sources on Debian (11+) / Ubuntu (20.04+).
+# This bash script compiles PostgreSQL from sources on Debian (12+) / Ubuntu (22.04+).
 #
 # Only for test
 #
@@ -8,7 +8,7 @@
 
 
 echo "
-Script to compile PostgreSQL on Debian (11+) / Ubuntu (20.04+).
+Script to compile PostgreSQL on Debian (12+) / Ubuntu (22.04+).
 
 This script is adapted from official documentation at 
 
@@ -19,23 +19,25 @@ https://www.postgresql.org/docs/current/installation.html
 sleep 1
 
 
-read -r -p "Type the version you want (eg: 12.15, 15.3)": VERSION
+read -r -p "Type the version you want (eg: 14.8, 15.3)": VERSION
+
+read -r -p "Type postgresql install folder (eg: /opt/pgsql)": INSTALL_FOLDER
 
 MAJOR_VERSION=$(cut -c 1-2 <<< "$VERSION")
 
-BUILD_FOLDER=$HOME/builds
+sudo mkdir -p "${INSTALL_FOLDER}" 
 
-mkdir -p "${BUILD_FOLDER}"
+BUILD_FOLDER="${INSTALL_FOLDER}"/builds
 
-cd "${BUILD_FOLDER}"/ || exit
+sudo mkdir -p "${BUILD_FOLDER}"
 
-
+cd "${BUILD_FOLDER}"/ || return
 
 # Install required packages
 
 sudo apt-get -y install  bison flex llvm clang zlib1g-dev \
 lib{ssl,systemd,readline,xslt1,xml2}-dev m4 make autoconf \
-pkgconf flex gcc make guile-2.2-dev patch automake  python3-dev
+pkgconf flex gcc make guile-3.0-dev patch automake  python3-dev
 
 # Download sources and extract the tar.gz file
 
@@ -48,7 +50,7 @@ cd postgresql-"$VERSION" || return
 # Run the configuration and make
 
 CXX=/usr/bin/g++ PYTHON=python3 ./configure \
---prefix=/opt/pgsql/"$MAJOR_VERSION" \
+--prefix="${INSTALL_FOLDER}"/"$MAJOR_VERSION" \
 --with-pgport=5435 \
 --with-python \
 --with-openssl \
@@ -74,8 +76,8 @@ else
     echo "postgres user already created"
 fi
 
-# Make postgres user the owner of /opt/pgsql folder
+# Make postgres user the owner of "${INSTALL_FOLDER}" folder
 
-sudo chown -R postgres:postgres /opt/pgsql
+sudo chown -R postgres:postgres "${INSTALL_FOLDER}"
 
 echo "Instalation finished!!" 
