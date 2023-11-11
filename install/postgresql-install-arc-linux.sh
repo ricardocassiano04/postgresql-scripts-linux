919f855ee58ad461bb95f97070460f332302a2bf
Ricardo Cassiano <rc.cassiano04@gmail.com>
2023-11-10 22:42:50 -0300

    The script now ask for fuser to write desired folder instalation

--- install/postgresql-install-arc-linux.sh
+++ install/postgresql-install-arc-linux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# This bash script compiles PostgreSQL from sources on Arch Linux.
+# This bash script compiles PostgreSQL from sources on Arch Linux and its derivatives.
 #
 # Author: Ricardo Cassiano
 
 echo '
-Script to compile PostgreSQL on Arch Linux.
+Script to compile PostgreSQL on Arch Linux and its derivatives.
 
 This script is adapted from official documentation at 
 
@@ -14,7 +14,9 @@ https://www.postgresql.org/docs/current/installation.html
 '
 # Ask user for desired version
 
-read -r -p "Type the version you want (eg: 12.15, 15.3)": VERSION
+read -r -p "Type the version you want (eg: 15.5, 16.1)": VERSION
+
+read -r -p "Type postgresql install folder (eg: /opt/pgsql )": INSTALL_FOLDER
 
 # Use only major version number for install location
 
@@ -38,7 +40,7 @@ cd postgresql-"$VERSION" || return
 
 
 CXX=/usr/bin/g++ PYTHON=python3 ./configure \
---prefix=/usr/local/pgsql/"$MAJOR_VERSION" \
+--prefix="${INSTALL_FOLDER}"/"{$MAJOR_VERSION}" \
 --with-pgport=5433 \
 --with-python \
 --with-openssl \
@@ -66,47 +68,49 @@ else
     echo "postgres user already created"	
 fi
 
-# Make postgres user the owner of /usr/local/pgsql folder
+# Make postgres user the owner of "${INSTALL_FOLDER}" folder
 
-sudo chown -R postgres:postgres /usr/local/pgsql
+sudo chown -R postgres:postgres "${INSTALL_FOLDER}"
 
 # Verifiy if already exists a postgresql installation. 
 # If not, configure binaries, manual and library path.
 
 command -v psql >/dev/null 2>&1 || \
 { sudo tee -a /etc/profile.d/pgsql.sh>>/dev/null<<EOF
-LD_LIBRARY_PATH=/usr/local/pgsql/"$MAJOR_VERSION"/lib
+LD_LIBRARY_PATH="${INSTALL_FOLDER}"/"{$MAJOR_VERSION}"/lib
 export LD_LIBRARY_PATH
 
-PATH=/usr/local/pgsql/"$MAJOR_VERSION"/bin:$PATH
+PATH="${INSTALL_FOLDER}"/"{$MAJOR_VERSION}"/bin:$PATH
 export PATH
 
-MANPATH=/usr/local/pgsql/"$MAJOR_VERSION"/share/man:$MANPATH
+MANPATH="${INSTALL_FOLDER}"/"{$MAJOR_VERSION}"/share/man:$MANPATH
 export MANPATH
 EOF
-sudo /sbin/ldconfig /usr/local/pgsql/"$MAJOR_VERSION"/lib
+sudo /sbin/ldconfig "${INSTALL_FOLDER}"/"{$MAJOR_VERSION}"/lib
 sudo chmod +x /etc/profile.d/pgsql.sh
 exit 1; }
 
 
 # Remove postgresql systemd service if it exists and create a new one.
 
-sudo rm  -f /etc/systemd/system/postgresql"$MAJOR_VERSION".service
+sudo rm  -f /etc/systemd/system/postgresql"{$MAJOR_VERSION}".service
 
-sudo tee -a /etc/systemd/system/postgresql"$MAJOR_VERSION".service>>/dev/null<<EOF
+sudo tee -a /etc/systemd/system/postgresql"{$MAJOR_VERSION}".service>>/dev/null<<EOF
 
 [Unit]
-Description=PostgreSQL "$MAJOR_VERSION" database server
+Description=PostgreSQL "{$MAJOR_VERSION}" database server
 Documentation=man:postgres(1)
+After=network-online.target
+Wants=network-online.target
 
 [Service]
 Type=notify
 User=postgres
-ExecStart=/usr/local/pgsql/"$MAJOR_VERSION"/bin/postgres -D /usr/local/pgsql/"$MAJOR_VERSION"/data
+ExecStart="${INSTALL_FOLDER}"/"{$MAJOR_VERSION}"/bin/postgres -D "${INSTALL_FOLDER}"/"{$MAJOR_VERSION}"/data
 ExecReload=/bin/kill -HUP $MAINPID
 KillMode=mixed
 KillSignal=SIGINT
-TimeoutSec=0
+TimeoutSec=infinity
 
 [Install]
 WantedBy=multi-user.target
@@ -114,12 +118,14 @@ EOF
 
 # Disable postgresql systemd service.
 
-sudo systemctl disable postgresql"$MAJOR_VERSION".service
+sudo systemctl disable postgresql"{$MAJOR_VERSION}".service
 
 sudo systemctl daemon-reload
 
 
 echo "Compilation finished. 
+
+Note: Remenber to run initdb before start the systemd service!
 
 By default, the postgresql$MAJOR_VERSION service is disabled.
 
