3e2c09e8aa87ebc76c30b015e9439206be782f9d
ricardocassiano04 <rc.cassiano04@gmail.com>
2025-05-29 23:37:21 -0300

    Queries para lidar com transações ociosas

--- sql/admin-tasks/cancel_idle_transactions_postgresql.sql
+++ sql/admin-tasks/cancel_idle_transactions_postgresql.sql
@@ -1,7 +1,38 @@
+--
+-- transactions not active
+--
+
+select
+  datname, pid, application_name, state_change, substring(query, 0, 60)
+from
+  pg_stat_activity
+where
+  state in ('idle', 'idle in transaction', 'idle in transaction (aborted)', 'disabled')
+  and pid <> pg_backend_pid()
+  and application_name <> 'psql'
+  and datname <> 'postgres'
+  and state_change <= now() - interval '15min';
+
+
+-- kill transactions not active
+
+
+
 select
   pg_terminate_backend(pid)
 from
   pg_stat_activity
 where
-  state = 'idle'
-  and query_start <= now() - interval '180min';
+  state in ('idle', 'idle in transaction', 'idle in transaction (aborted)', 'disabled')
+  and pid <> pg_backend_pid()
+  and application_name <> 'psql'
+  and datname <> 'postgres'
+  and state_change <= now() - interval '15min';
+  
+
+-- kill a especifi pid
+ 
+SELECT pg_cancel_backend(pid);
+
+select pg_terminate_backend(pid); -- force
+
