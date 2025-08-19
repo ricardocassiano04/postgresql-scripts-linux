7ce35ae824ec56f718b8d094a5fc3b045cbda12e
ricardocassiano04 <rc.cassiano04@gmail.com>
2025-08-18 21:04:01 -0300

    Validação de versão do debian

---
+++ install/postgresql-install-upstream-pgadmin.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# Configura o repositório e instala o PgAdmin (desktop) no Debian 12 / Ubuntu e Linux Mint.
+# 
+#
+# Autor: Ricardo Cassiano
+#
+#
+
+
+echo "
+ 
+Verificando se sua ditribuição é compatível com o script...
+
+"
+
+sleep 1
+
+# Verificar a distruição
+
+source /etc/os-release
+
+if [[ "$ID" == "linuxmint" || "$ID" == "ubuntu" ]]; then
+   distro="$UBUNTU_CODENAME"
+   echo "Sua distro $PRETTY_NAME é suportada. Iniciando a instalação..."
+   
+elif [[ "$ID" == "ubuntu" ]]; then
+   distro="$VERSION_CODENAME"
+   echo "Sua distro $PRETTY_NAME é suportada. Iniciando a instalação..."
+   
+elif [[ "$ID" == "debian"  ]]; then
+	versao=${VERSION_ID%%.*}
+	
+	if (( versao == 13 || versao < 11 )); then
+		echo "Sua distro $PRETTY_NAME não é suportada por este script. Saindo..."
+		exit 0
+	elif (( versao == 12 )); then
+		distro="$VERSION_CODENAME"
+		echo "Sua distro $PRETTY_NAME é suportada. Iniciando a instalação..."
+	fi
+fi
+
+
+
+
+echo "
+ 
+ Configura o repositório e instala o PgAdmin (desktop) no Debian 12 / Ubuntu e Linux Mint.
+
+
+
+PgAdmin
+
+https://www.pgadmin.org/download/pgadmin-4-apt/
+
+"
+
+sleep 1
+	
+
+#  Instalar os pacotes necessários para configurar os repositótios
+
+sudo apt-get -y install curl lsb-release ca-certificates
+
+# Importa a chave do repositório 
+
+curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+
+
+# Adicionar repositórios
+
+sudo tee -a /etc/apt/sources.list.d/pgadmin.list>>/dev/null<<EOF
+
+deb [arch=amd64 signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/"${distro}" pgadmin4 main
+
+EOF
+
+
+# Atualiza repositórios e instala os pacotes
+
+sudo apt-get update
+
+
+sudo apt-get -y install pgadmin4-desktop 
+
+
+echo "Instalação finalizada!!"
+
+
--- install/postgresql-install-upstream-repository.sh
+++ install/postgresql-install-upstream-repository.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Configura o repositório e instala o PostgreSQL e PgAdmin (desktop) no Debian.
+# Configura o repositório e instala o PostgreSQL no Debian 11+,  Ubuntu / Linux Mint.
 # 
 #
 # Autor: Ricardo Cassiano
@@ -12,16 +12,48 @@
 
 echo "
  
- Configura o repositório e instala o PostgreSQL e PgAdmin (desktop) no Debian 12+ / Ubuntu e derivados.
+Verificando se sua ditribuição é compatível com o script...
+
+"
+
+sleep 3
+
+
+# Verificar a distruição
+
+source /etc/os-release
+
+if [[ "$ID" == "linuxmint" || "$ID" == "ubuntu" ]]; then
+   distro="$UBUNTU_CODENAME"
+   echo "Sua distro $PRETTY_NAME é suportada. Iniciando a instalação..."
+   
+elif [[ "$ID" == "ubuntu" ]]; then
+   distro="$VERSION_CODENAME"
+   echo "Sua distro $PRETTY_NAME é suportada. Iniciando a instalação..."
+   
+elif [[ "$ID" == "debian"  ]]; then
+	versao=${VERSION_ID%%.*}
+	
+	if ((  versao < 11 )); then
+		echo "Sua distro $PRETTY_NAME não é suportada por este script. Saindo..."
+		exit 0
+	elif (( versao > 10 )); then
+		distro="$VERSION_CODENAME"
+		echo "Sua distro $PRETTY_NAME é suportada. Iniciando a instalação..."
+	fi
+fi
+
+
+
+echo "
+ 
+ Configura o repositório e instala o PostgreSQL no Debian 11+,  Ubuntu / Linux Mint.
 
 
 https://www.postgresql.org/download/linux/debian/
 
-PgAdmin
-
-https://www.pgadmin.org/download/pgadmin-4-apt/
-
 "
+
 
 sleep 1
 
@@ -31,17 +63,6 @@ read -r -p "Digite a versão que você quer instalar (ex: 15, 16, 17)": VERSAO
 
 sudo apt-get -y install curl wget lsb-release ca-certificates
 
-
-
-# Verificar se a distruição é Linux Mint para poder usar o codinome do Debian
-
-if [ "$(grep -E '^ID=' /etc/os-release)" = "ID=linuxmint" ]; then
-	distro=$(grep -Po '(?<=UBUNTU_CODENAME=)\w+' /etc/os-release)	
-else
-    distro=$(grep -Po '(?<=VERSION_CODENAME=)\w+' /etc/os-release)
-fi
-
-
 # Importa a chave do repositório 
 
 sudo apt install curl ca-certificates
@@ -50,7 +71,6 @@ sudo install -d /usr/share/postgresql-common/pgdg
 
 sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
 
-curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 
 
 # Adicionar repositórios
@@ -59,7 +79,6 @@ sudo tee -a /etc/apt/sources.list.d/pgsql.list>>/dev/null<<EOF
 
 deb [arch=amd64 signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] http://apt.postgresql.org/pub/repos/apt "${distro}"-pgdg main
 
-deb [arch=amd64 signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/"${distro}" pgadmin4 main
 
 EOF
 
@@ -70,7 +89,7 @@ sudo apt-get update
 
 sudo apt-get install postgresql-client-"${VERSAO}"
 
-sudo apt-get -y install pgadmin4-desktop postgresql-"${VERSAO}" \
+sudo apt-get -y install postgresql-"${VERSAO}" \
 postgresql-doc-"${VERSAO}" postgresql-plpython3-"${VERSAO}" \
 postgresql-server-dev-"${VERSAO}" pg-activity
 
