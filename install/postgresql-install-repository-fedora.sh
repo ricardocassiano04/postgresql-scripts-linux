d677bdbea13b84f223846c136b8114bd9951cd0d
Ricardo Cassiano <rc.cassiano04@gmail.com>
2025-10-09 22:51:28 -0300

    Script para o Fedora Linux

---
+++ install/postgresql-install-repository-fedora.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Configura o repositório e instala o PostgreSQL no Fedora Linux
+# 
+#
+# Autor: Ricardo Cassiano
+#
+# Referência: https://www.postgresql.org/download/linux/redhat/
+#
+#
+
+# Verificar a distribuição
+
+source /etc/os-release
+
+if [[ "$ID" != "fedora"  ]]; then   
+   echo "Sua distro $PRETTY_NAME não é suportada. Saindo..."
+   exit 0
+   
+elif [[ "$ID" == "fedora"  ]]; then
+	versao=${VERSION_ID%%.*}
+	
+	if ((  versao < 41 )); then
+		echo "Sua distro $PRETTY_NAME não é suportada por este script. Saindo..."
+		exit 0
+	elif (( versao > 40 )); then		
+		echo "Sua distro $PRETTY_NAME é suportada. Iniciando a instalação..."
+	fi
+fi
+
+
+echo "
+
+Configurando o repositório e instalando o PostgreSQL no Fedora Linux
+
+"
+
+sleep 2
+
+
+read -r -p "Digite a versão que você quer instalar (ex: 16, 17, 18)": VERSAO_POSTGRES
+
+
+echo "Comandos de configurações conforme documentação em https://www.postgresql.org/download/linux/redhat/"
+
+sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/F-"${versao}"-x86_64/pgdg-fedora-repo-latest.noarch.rpm
+
+sudo dnf install -y postgresql"${VERSAO_POSTGRES}"-server
+
+sudo /usr/pgsql-"${VERSAO_POSTGRES}"/bin/postgresql-"${VERSAO_POSTGRES}"-setup initdb
+sudo systemctl enable postgresql-"${VERSAO_POSTGRES}"
+sudo systemctl start postgresql-"${VERSAO_POSTGRES}"
+
+
+echo "Instalação finalizada!!"
